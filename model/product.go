6c84b1686f07fdd6d75cfd511db5aeaa6f38812f
SamirMamede <anuarsamir@gmail.com>
2024-08-29 23:50:55 -0400

    model product struct

--- model/product.go
+++ model/product.go
@@ -1,4 +1,9 @@
 package model
 
 type Product struct {
+	ID          int     `json:"id_product"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	ImagePath   string  `json:"image_path"`
 }
