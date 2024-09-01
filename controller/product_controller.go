98ad1684684ddd478f57afcf4c86b5cdfa1da781
SamirMamede <anuarsamir@gmail.com>
2024-08-31 23:31:54 -0400

    update product controller

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -1,5 +1,15 @@
 package controller
 
+import "github.com/gin-gonic/gin"
+
 type productController struct {
 	//Usecase
+}
+
+func NewProductController() productController {
+	return productController{}
+}
+
+func (p *productController) GetProducts(ctx *gin.Context) {
+	//list products
 }
